68160409d31dbbb3efd25210d874efacbbb148a8
Lootiano <lucianograbin12@gmail.com>
2024-02-02 06:14:47 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+var todo = []
+
+function get() {
+    console.log(todo)
+}
+
+function getData() {
+    return data = document.getElementById('task').value
+}
+
+function post() {
+    todo.push(getData())
+}
+
+get()
